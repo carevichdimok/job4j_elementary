155ddedd6ab186b4406663090ca9053eee05a038
carevichdimok <91655383+carevichdimok@users.noreply.github.com>
2021-10-08 19:10:25 +0500

    Add files via upload

--- src/main/java/ru/job4j/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/ru/job4j/calculator/MathFunc.java
@@ -3,7 +3,7 @@ package ru.job4j.ru.job4j.calculator;
 public class MathFunc {
 
     public static int func1(int x) {
-        int y = x * x + 1 ;
+        int y = x * x + 1;
         return y;
     }
 
